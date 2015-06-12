7fffb704e431ded9bb85e8b02cdc127124120266
Sarah R <phoesarah@hotmail.com>
2015-06-12 14:21:02 -0500

    store project

---
+++ ConsoleApplication1/ClassLibrary1/Products.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Products
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public override string ToString()
+        {
+            return (Name +" costs " + Price);
+        }
+
+        public Products(decimal price, string name)
+        {
+            Price = price;
+            Name = name;
+        }
+      
+    }
+    
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool quit = false;
+            while (quit != true)
+            {
+                Showmenu();
+                switch (Console.ReadLine().Trim().ToLower())
+                {
+                    case "p":
+                        {
+                            Products product = new Products(13.00m, "knife");
+                            Console.WriteLine(product);
+                            break;
+                        }
+                    case "q":
+                        {
+                            quit = true;
+                            break;
+                        }
+
+                }
+            }
+           
+        }
+
+        private static void Showmenu()
+        {
+           Console.WriteLine("Press Q to Quit");
+           Console.WriteLine("Press P to see Products");
+           
+        }
+    }
+}
